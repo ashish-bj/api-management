f5e1390430a7dbb43a19b6fc6b7327167763a81a
abjadhav <144044284+ashish-bj@users.noreply.github.com>
2025-04-07 22:26:24 +0530

    Update pranalysis.py

--- pranalysis.py
+++ pranalysis.py
@@ -2,7 +2,7 @@ import os
 import requests
 import re
 from langchain_openai import AzureChatOpenAI
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 
 api_key = os.getenv("AZURE-OPENAI-API-KEY")
 azure_endpoint = os.getenv("AZURE-OPENAI-ENDPOINT")

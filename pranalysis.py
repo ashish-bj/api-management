cd946544e967dc7aacdbb1d25816a9457a02fff3
abjadhav <144044284+ashish-bj@users.noreply.github.com>
2025-04-07 22:13:24 +0530

    Update pranalysis.py

--- pranalysis.py
+++ pranalysis.py
@@ -49,8 +49,8 @@ if response.status_code == 200:
 llm = AzureChatOpenAI(
     azure_deployment="gpt-4o",  # or your deployment
     api_version="2024-12-01-preview",  # or your api version
-    api_key=os.environ["AZURE_OPENAI_API_KEY"],
-    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    api_key=os.environ["AZURE-OPENAI-API-KEY"],
+    azure_endpoint=os.environ["AZURE-OPENAI-ENDPOINT"],
     temperature=0,
     max_tokens=None,
     timeout=None,

0cc17eeb779a34ab7c2adf72664e7cdc2221c0e1
abjadhav <144044284+ashish-bj@users.noreply.github.com>
2025-04-08 21:45:08 +0530

    Update pranalysis.py

--- pranalysis.py
+++ pranalysis.py
@@ -67,7 +67,7 @@ messages = [
         content="You are a pull request reviwer!"
     ),
     HumanMessage(
-        content="You are a helpful assistant that review github pull requests. You analyze the code changes and provide insights. check for syntax errors, explain what changes are about & its impact, check for vulnerabilities or potential bug & also check for hardcoded passwords. provide summary in shortest possible format for codeowners to make decision of approve pull request or not"
+        content="You are a helpful assistant that review github pull requests. You analyze the code changes and provide insights. check for syntax errors, explain what changes are about & its impact, check for vulnerabilities or potential bug & also check for hardcoded passwords. provide summary in shortest possible format for codeowners to make decision of approve pull request or not. Depending upon critical issues observed, conclude summary with 'Merge Pull Reuest = Yes' or 'Merge Pull Request = No' "
     ),
     HumanMessage(content=diff_text),
 ]

6909832c0459cb68ae1a464df21f5fc9d6ae5aa5
abjadhav <144044284+ashish-bj@users.noreply.github.com>
2025-04-16 18:04:29 +0530

    Update pranalysis_automerge.py

--- pranalysis_automerge.py
+++ pranalysis_automerge.py
@@ -50,8 +50,8 @@ if response.status_code == 200:
 # )
        
 llm = AzureChatOpenAI(
-    azure_deployment="codellama-7b-hf-12",  # or your deployment
-    api_version="2024-12-01-preview",  # or your api version
+    azure_deployment="DeepSeek-R1",  # or your deployment
+    api_version="1",  # or your api version
     api_key=os.environ["AZURE-OPENAI-API-KEY"],
     azure_endpoint=os.environ["AZURE-OPENAI-ENDPOINT"],
     temperature=0,
